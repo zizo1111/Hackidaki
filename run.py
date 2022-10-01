7d6ade4f48f6cb04f0e80ef36f79972a5b59c9dd
ozayed <omar.zayed@tum.de>
2022-10-01 12:07:05 +0200

    first implementation

---
+++ run.py
@@ -0,0 +1,19 @@
+from src.text_extractor import TextExractor
+from src.detoxify.detoxify.detoxify import Detoxify
+
+
+class HateRecognizer:
+    def __init__(self) -> None:
+        self.hate_predictor = Detoxify('unbiased')
+        self.text_extractor = TextExractor()
+    
+    def predict_hate(self, text):
+        return self.hate_predictor.predict(text)
+    
+    def predict_hate_img(self, img):
+        extracted_text = self.text_extractor.recognize_text(img, True)
+        return self.hate_predictor.predict(extracted_text)
+
+if __name__ == "__main__":
+    hr = HateRecognizer()
+    print(hr.predict_hate_img('hate_examples/hate_example2.jpeg'))
---
+++ src/text_extractor.py
@@ -0,0 +1,26 @@
+
+from cgitb import text
+import numpy as np
+import easyocr
+
+class TextExractor:
+
+    def __init__(self):
+        self.reader = easyocr.Reader(['en'])
+
+    def recognize_text(self, img, conact = False):
+        '''loads an image and recognizes text.'''
+
+        result = self.reader.readtext(img)
+        return_arr = []
+        if result is not None and len(result) > 0:
+            for line in result:
+                return_arr.append(line[1])
+
+        if len(return_arr) > 0:
+            if conact:
+                return ' '.join(return_arr)
+            else:
+                return return_arr
+        else:
+            return None
